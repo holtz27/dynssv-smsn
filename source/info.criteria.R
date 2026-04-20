a92f3618b547746ea9b0aa57dcb958cafe3caf6e
holtz27 <97683235+holtz27@users.noreply.github.com>
2026-04-20 14:35:40 -0300

    Update info.criteria.R

--- source/info.criteria.R
+++ source/info.criteria.R
@@ -24,7 +24,7 @@ dic = function(data, mu_t, sigma_t){
   n_iter = dim(mu_t)[1]
   n_obs  = length(data)
   
-  # 1. Log-verossimilhança para cada iteração e tempo
+  # 1. Log-likelihood for each iteration and time
   loglik_matrix = matrix(NA, nrow = n_iter, ncol = n_obs)
   for(j in 1:n_obs){
     loglik_matrix[,j] = dnorm(data[j],
@@ -33,21 +33,21 @@ dic = function(data, mu_t, sigma_t){
                               log  = TRUE)
   }
   
-  # 2. Deviance média: \bar{D}
+  # 2. Average deviance: \bar{D}
   D_bar = -2 * mean(rowSums(loglik_matrix))
   
-  # 3. Parâmetros médios: \hat{\mu}_t, \hat{\sigma}_t
+  # 3. Average parameters: \hat{\mu}_t, \hat{\sigma}_t
   mu_hat     = colMeans(mu_t)
   sigma_hat  = colMeans(sigma_t)
   
-  # 4. Deviance na média posterior: D(\hat{\theta})
+  # 4. Deviance in the posterior mean: D(\hat{\theta})
   D_hat = -2 * sum(dnorm(data,
                          mean = mu_hat,
                          sd   = sigma_hat,
                          log  = TRUE))
-  # 5. Penalização de complexidade efetiva
+  # 5. Penalization for effective complexity
   p_D = D_bar - D_hat
-  # 6. Cálculo final do DIC
+  # 6. Final calculation of DIC
   dic = D_bar + p_D
   
   return(dic)
